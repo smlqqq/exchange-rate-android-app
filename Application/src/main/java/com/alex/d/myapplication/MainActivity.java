b78dc5e626aedd0f7adceaa1e37b93fc33884f7c
smlqqq <78303132+smlqqq@users.noreply.github.com>
2022-12-01 02:28:14 +0200

    adde toolbar

--- Application/src/main/java/com/alex/d/myapplication/MainActivity.java
+++ Application/src/main/java/com/alex/d/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView fourthView;
     private TextView fifthView;
     private TextView textData2;
+    private Toolbar mytoolbar;
 
 
 
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mytoolbar = findViewById(R.id.mytoolbar);
         firstView = findViewById(R.id.firstView);
         secondView = findViewById(R.id.secondView);
         thirdView = findViewById(R.id.thirdView);
