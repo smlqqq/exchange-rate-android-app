fc38e8af95652c6be379c546b2fdf1f9a8e0f79d
smlqqq <78303132+smlqqq@users.noreply.github.com>
2023-08-12 18:27:39 +0300

    add block gif

--- Application/src/main/java/com/alex/d/myapplication/ListItemClass.java
+++ Application/src/main/java/com/alex/d/myapplication/ListItemClass.java
@@ -81,13 +81,6 @@ public class ListItemClass {
 
 
 
-//    public ListItemClass(String data1, String data2, String data3, String image) {
-//        this.data1 = data1;
-//        this.data2 = data2;
-//        this.data3 = data3;
-//        this.imageUrl = image;
-//    }
-
     public ListItemClass() {
 
     }
