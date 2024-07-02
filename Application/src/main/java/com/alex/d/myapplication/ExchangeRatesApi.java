0e96063782423c2accc04fadcc259aad9e5a2e43
smlqqq <78303132+smlqqq@users.noreply.github.com>
2024-07-02 20:06:43 +0300

    minor fixes

--- Application/src/main/java/com/alex/d/myapplication/ExchangeRatesApi.java
+++ Application/src/main/java/com/alex/d/myapplication/ExchangeRatesApi.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ExchangeRatesApi {
-    @GET("/api/data")
+    @GET("/api/data/latest")
     Call<ExchangeRatesResponse> getExchangeRates();
 }
--- Application/src/main/java/com/alex/d/myapplication/ExchangeRatesResponse.java
+++ Application/src/main/java/com/alex/d/myapplication/ExchangeRatesResponse.java
@@ -17,4 +17,5 @@ public class ExchangeRatesResponse {
     public String getTimestamp() {
         return timestamp;
     }
+
 }
